37bcce74bd3d54629a5b8c23069aab1824c60722
Ala-Younes <ala.younes.ya@gmail.com>
2024-01-23 11:24:13 +0100

    0001 / UPDATE / Insert Dummy Data

--- components/WorkSlider.tsx
+++ components/WorkSlider.tsx
@@ -1,5 +1,51 @@
+// data
+const workSlides = {
+  slides: [
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+      ],
+    },
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+      ],
+    },
+  ],
+};
+
 const WorkSlider = () => {
-  return <></>;
+  return <div>Work Slider</div>;
 };
 
 export default WorkSlider;
