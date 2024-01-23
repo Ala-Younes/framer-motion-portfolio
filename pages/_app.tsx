2b8815cb1cab3301b184981b1beedbf899da8529
Ala-Younes <ala.younes.ya@gmail.com>
2024-01-23 12:50:11 +0100

    0004 / Feature / Transition using framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import Layout from "@/components/Layout";
+import Transition from "@/components/Transition";
 import "@/styles/globals.css";
+import { AnimatePresence, motion } from "framer-motion";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      {/* Is AnimationPresence necessary to trigger the animation ? whats the structure behind ? go to doc ... */}
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
