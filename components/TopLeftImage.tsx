79d14b604bab2d940263d543a29480d699f0fcbc
Ala-Younes <ala.younes.ya@gmail.com>
2024-01-23 11:42:58 +0100

    0002 / Feature / Layout + insert background image with a blend prop

--- components/TopLeftImage.tsx
+++ components/TopLeftImage.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import topLeftImage from "@/public/top-left-img.png";
+
 const TopLeftImage = () => {
-  return <h1>TopLeftImage</h1>;
+  // ! mix-blend-color-dodge => try lighten or darken
+  return (
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+      <Image src={topLeftImage} width={400} height={400} alt="top left image" />
+    </div>
+  );
 };
 
 export default TopLeftImage;
