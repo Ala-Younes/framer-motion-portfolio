6f5b7c53dc861fabd3ce790113c6ab055a432442
Ala-Younes <ala.younes.ya@gmail.com>
2024-01-24 12:03:18 +0100

    0007 / Feature / Header(Socials + logo)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
+import Logo from "@/public/logo.svg";
+import Socials from "./Socials";
 const Header = () => {
-  return <header className="absolute z-30 w-full ">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row  items-center justify-between gap-y-6 py-8">
+          {/* Logo */}
+          <Link href={"/"}>
+            <h2 className="font-extrabold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-accent/50 via-orange-600/70 to-accent">
+              Ala Younes
+            </h2>
+          </Link>
+          {/* Socials */}
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+
+import {
+  RiBehanceLine,
+  RiFacebookBoxLine,
+  RiFacebookLine,
+  RiInstagramLine,
+  RiPinterestLine,
+  RiYoutubeLine,
+} from "react-icons/ri";
 const Socials = () => {
-  return <h1>Socials</h1>;
+  return (
+    <div className="flex items-center gap-x-5 text-2xl">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiBehanceLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiPinterestLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
